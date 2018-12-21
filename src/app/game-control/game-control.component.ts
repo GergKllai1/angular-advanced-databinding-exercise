596f39aeb697e76d34f23f97023b1c4f5f647d4c
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-21 11:14:27 +0100

    Adds start game function which increases the score every second

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  score: number = 0
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  startGame() {
+    setInterval( () => {
+      let i = this.score;
+      i ++;
+      this.score = i;
+    }, 1000)
+  }
 }
